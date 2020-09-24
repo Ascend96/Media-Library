2179855426a2db3df42fb09c850bf29e9f69e856
Ascend96 <mmartens28@yahoo.com>
2020-09-23 20:14:28 -0500

    Input genres
    - Add constructor to Movie class (initialize genres List)
    - Input genres until “done” is entered
    - Specify if no genres entered

--- MovieLibrary/Movie.cs
+++ MovieLibrary/Movie.cs
@@ -9,6 +9,13 @@ namespace MovieLibrary
         public UInt64 movieId { get; set; }
         public string title { get; set; }
         public List<string> genres { get; set; }
+
+        // constructor
+        public Movie()
+        {
+            genres = new List<string>();
+        }
+
                 public string Display()
         {
             return $"Id: {movieId}\nTitle: {title}\nGenres: {string.Join(", ", genres)}\n";

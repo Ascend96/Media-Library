162b5fe989241ec9336ed016bb14ffe8f27cc661
Ascend96 <mmartens28@yahoo.com>
2020-10-02 17:44:52 -0500

    Added Menu Displays all movies Added Director, Running time

---
+++ MediaLibrary/MediaLibrary/MediaFile.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using NLog.Web;
+using System.Linq;
+
+namespace MediaLibrary
+{
+
+    public class MovieFile {
+
+        public string filePath { get; set; }
+
+        public List<Movie> Movies { get; set; }
+
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+
+        public MovieFile(string movieFilePath){
+            filePath = movieFilePath;
+            Movies = new List<Movie>();
+
+            try {
+                StreamReader sr = new StreamReader(filePath);
+                sr.ReadLine();
+                while(!sr.EndOfStream){
+                    Movie movie = new Movie();
+                    string line = sr.ReadLine();
+
+                    int idx = line.IndexOf('"');
+                    if(idx == -1){
+
+                        string[] movieDetails = line.Split(',');
+                        movie.mediaId = UInt64.Parse(movieDetails[0]);
+                        movie.title = movieDetails[1];
+                        movie.genres = movieDetails[2].Split('|').ToList();
+                        movie.director = movieDetails[3];
+                        movie.runningTime = TimeSpan.Parse(movieDetails[4]); 
+                    }
+                    else{
+                        movie.mediaId = UInt64.Parse(line.Substring(0, idx - 1));
+
+                        line = line.Substring(idx + 1);
+
+                        idx = line.IndexOf('"');
+
+                        movie.title = line.Substring(0, idx);
+
+                        line = line.Substring(idx + 2);
+
+                        string [] details = line.Split(',');
+
+                        movie.genres = details[0].Split('|').ToList();
+                        
+
+                     //   movie.director = details.Length > 1 ? details[1] : "unassigned";
+
+                    //    movie.runningTime = details.Length > 2 ? TimeSpan.Parse(details[2]) : new TimeSpan(0);
+
+                     movie.director = "unassigned";
+
+                     //   movie.runningTime = new TimeSpan(2, 22, 22);
+                    }
+                    Movies.Add(movie);
+
+                }
+                sr.Close();
+                logger.Info("Movies in file {count}", Movies.Count);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+        }
+
+        public void AddMovie(Movie movie){
+            try{
+                movie.mediaId = Movies.Max(m => m.mediaId) + 1;
+
+                StreamWriter sw = new StreamWriter(filePath, true);
+                sw.WriteLine($"{movie.mediaId}, {movie.title},{string.Join('|', movie.genres)}, {movie.director}, {movie.runningTime}");
+                sw.Close();
+
+                Movies.Add(movie);
+
+                logger.Info("Movie id {Id} added", movie.mediaId);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+            
+
+        }
+    }
+}
--- MediaLibrary/MediaLibrary/Program.cs
+++ MediaLibrary/MediaLibrary/Program.cs
@@ -13,15 +13,65 @@ namespace MediaLibrary
         {
             logger.Info("Program started");
 
-                                     Movie movie = new Movie
-             {
-                 mediaId = 123,
-                title = "Greatest Movie Ever, The (2020)",
-                director = "Jeff Grissom",
-                // timespan (hours, minutes, seconds)
-                runningTime = new TimeSpan(2, 21, 23),
-                genres = { "Comedy", "Romance" }
-             };
+            string movieFilePath = "movies.scrubbed.csv";
+
+                                     MovieFile movieFile = new MovieFile(movieFilePath);
+
+                                     string choice = "";
+
+            do {
+                Console.WriteLine("1) Add Movie");
+                Console.WriteLine("2) Display all movies");
+                Console.WriteLine("Enter to quit");
+
+                choice = Console.ReadLine();
+               logger.Info("User choice: {choice}", choice);
+                if(choice == "1"){
+                    Movie movie = new Movie();
+                    Console.WriteLine("Enter movie title");
+
+                    movie.title = Console.ReadLine();
+
+                    string input;
+
+                    do {
+                        Console.WriteLine("Enter genre (or done to quit)");
+
+                        input = Console.ReadLine();
+
+                        if(input != "done" && input.Length > 0)
+                        {
+                            movie.genres.Add(input);
+                        }
+                    } while (input != "done");
+                        if (movie.genres.Count == 0){
+                            movie.genres.Add("no genres listed");
+                        }
+                        Console.WriteLine("Enter Directors name");
+                        movie.director = Console.ReadLine();
+
+                        Console.WriteLine("Enter running time (h:m:s)");
+                        movie.runningTime = TimeSpan.Parse(Console.ReadLine());
+                        movieFile.AddMovie(movie);
+
+                } else if (choice == "2"){
+                    foreach(Movie m in movieFile.Movies){
+
+                        Console.WriteLine(m.Display());
+                    }
+                }
+            }  while (choice == "1" || choice == "2");
+
+
+
+          //   {
+           //      mediaId = 123,
+           //     title = "Greatest Movie Ever, The (2020)",
+           //     director = "Jeff Grissom",
+           //     // timespan (hours, minutes, seconds)
+           //     runningTime = new TimeSpan(2, 21, 23),
+         //       genres = { "Comedy", "Romance" }
+          //   };
              
 
             // Console.WriteLine(movie.Display());
@@ -49,6 +99,8 @@ namespace MediaLibrary
 
             string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
             logger.Info(scrubbedFile);
+
+            
 
             logger.Info("Program ended");
         }
